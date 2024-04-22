e73c05b6a5d2558eee9a815261bc059142d53df5
Daniel Acosta <daniel.acosta@sinco.com.co>
2024-04-22 09:23:33 -0500

    crear repositorio y hacer commit

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+import  '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}><h1 className="text-blue-500">I'm blue!</h1>{children}</body>
+    </html>
+  );
+}
