e703d98e8ba2f057185da499f5c8bcb98bf7a125
Daniel Acosta <daniel.acosta@sinco.com.co>
2024-04-24 10:28:01 -0500

    CardWrapper

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -15,23 +15,25 @@ const iconMap = {
 };
 
 export default async function CardWrapper() {
+
+  const { totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers } = await fetchCardData();
   return (
     <>
       {/* NOTE: comment in this code when you get to this point in the course */}
 
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+       <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
         title="Total Customers"
         value={numberOfCustomers}
         type="customers"
-      /> */}
+      /> 
     </>
   );
 }
 
-export async function Card({
+export function Card({
   title,
   value,
   type,
@@ -41,11 +43,6 @@ export async function Card({
   type: 'invoices' | 'customers' | 'pending' | 'collected';
 }) {
   const Icon = iconMap[type];
-  
-  if (value == 'tPI'){
-    const { totalPaidInvoices } = await fetchCardData();
-    value = totalPaidInvoices;
-  }
 
   return (
     <div className="rounded-xl bg-gray-50 p-2 shadow-sm">
